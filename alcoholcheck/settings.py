c243d77f3669f15f7667c20c80ac120b91acc210
username <tkenshi19@gmail.com>
2022-09-19 20:43:14 +0900

    環境変数に基ずくDEBUGの設定

--- alcoholcheck/settings.py
+++ alcoholcheck/settings.py
@@ -127,3 +127,8 @@ LOGIN_URL = 'users:login'
 #Heroku の設定
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
