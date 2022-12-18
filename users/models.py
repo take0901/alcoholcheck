8efbe38e1b2017a316080f9eadc620d7541c0369
username <tkenshi19@gmail.com>
2022-12-18 17:36:19 +0900

    max_length削除

--- users/models.py
+++ users/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    catnumber = models.IntegerField(max_length=10)
+    catnumber = models.IntegerField()
