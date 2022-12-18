9de2b8b364389a19daccf3ad81f26467a98be0f0
username <tkenshi19@gmail.com>
2022-12-18 17:34:19 +0900

    スタムユーザーを追加

--- users/models.py
+++ users/models.py
@@ -2,5 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    class Meta:
-        db_table = 'auth_user'
+    catnumber = models.IntegerField(max_length=10)
