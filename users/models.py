66ec5f1eca1aa9e2b4806d47cf9d12edf432c98a
username <tkenshi19@gmail.com>
2022-12-18 17:46:53 +0900

    user属性のミスを直した

--- users/models.py
+++ users/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    catnumber = models.IntegerField()
+    carnumber = models.IntegerField()
