fb7e7e10b6aa100f1693fc3dd7399b2b574591a5
username <tkenshi19@gmail.com>
2024-12-23 20:48:00 +0900

    userのパスワードのハッシュ化

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from .models import User
-
-admin.site.register(User)
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.contrib.auth.forms import UserChangeForm
+from users.models import User
+ 
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+ 
+class CustomUserAdmin(DefaultUserAdmin):
+    form = CustomUserChangeForm
+    add_form = DefaultUserAdmin.add_form
+    model = User
+    add_fieldsets = DefaultUserAdmin.add_fieldsets
+    add_fieldsets[0][1]['fields'] += ('carnumber',)
+ 
+admin.site.register(User, CustomUserAdmin)
